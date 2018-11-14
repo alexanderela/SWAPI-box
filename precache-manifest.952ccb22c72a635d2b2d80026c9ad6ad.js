f2adf3b108195749638e9ba4938715f673ddcd3f
Dina <dcaraballo26@gmail.com>
2018-11-14 13:30:26 -0700

    Updates

---
+++ precache-manifest.952ccb22c72a635d2b2d80026c9ad6ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5bc7f4f05bc799233d45b4a35fedb09",
+    "url": "/SWAPI-box/static/media/swapi_rough_gray.a5bc7f4f.jpg"
+  },
+  {
+    "revision": "5d6a625d6647c487bb97e7eeee4ceae9",
+    "url": "/SWAPI-box/static/media/SWAPI-background.5d6a625d.jpg"
+  },
+  {
+    "revision": "ade91cb0bb7c69696a7df18125ae82cb",
+    "url": "/SWAPI-box/static/media/Death Star.ade91cb0.otf"
+  },
+  {
+    "revision": "05ffcd0487c4c1e751b5",
+    "url": "/SWAPI-box/static/js/runtime~main.05ffcd04.js"
+  },
+  {
+    "revision": "7541cd67e50039b1a49f",
+    "url": "/SWAPI-box/static/js/main.7541cd67.chunk.js"
+  },
+  {
+    "revision": "959f49a99fd41ad12038",
+    "url": "/SWAPI-box/static/js/1.959f49a9.chunk.js"
+  },
+  {
+    "revision": "7541cd67e50039b1a49f",
+    "url": "/SWAPI-box/static/css/main.b938c13f.chunk.css"
+  },
+  {
+    "revision": "4bdc24c5b252fb825e09278c881df5b1",
+    "url": "/SWAPI-box/index.html"
+  }
+];
